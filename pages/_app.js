5c0a1af7522f013a20472e471edb2894ca6bacd3
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-10-12 15:22:31 +0530

    feat: add near testnet to wallet context

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,21 @@ import { WalletContextProvider } from "@mintbase-js/react";
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { Tooltip } from "react-tooltip";
+import { NEAR_NETWORKS, Network, mbjs } from "@mintbase-js/sdk";
 
 function MyApp({ Component, pageProps }) {
+  const network = process.env.NEXT_PUBLIC_NETWORK || NEAR_NETWORKS.TESTNET;
+  const nearNetwork = network || NEAR_NETWORKS.TESTNET;
+
+  // also the store you should be a minter
+  const contractAddress =
+    process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "mystore.mintbase1.near";
+
+  mbjs.config({
+    network: nearNetwork,
+    contractAddress: contractAddress,
+  });
+
   return (
     <>
       <Tooltip id="replicate-tooltip" />
@@ -11,7 +24,10 @@ function MyApp({ Component, pageProps }) {
       <Tooltip id="bytescale-tooltip" />
       <Tooltip id="github-tooltip" />
       <Tooltip id="youtube-tooltip" />
-      <WalletContextProvider>
+      <WalletContextProvider
+        contractAddress={contractAddress}
+        network={nearNetwork}
+      >
         <Component {...pageProps} />
       </WalletContextProvider>
       <Analytics />

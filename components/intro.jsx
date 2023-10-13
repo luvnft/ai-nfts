eaad2cd473cac5156dee5e2ebb2eac110bade5bb
0xyshv <writetoyshv@gmail.com>
2023-10-13 22:27:59 +0530

    fix:sync changes

--- components/intro.jsx
+++ components/intro.jsx
@@ -11,10 +11,14 @@ function Intro() {
   const buttonAction = isConnected ? disconnect : connect;
 
   return (
-    <nav className="flex justify-between w-full bg-white sticky top-0 z-40 lg:border-b border-solid border-gray-150">
+    <nav className="flex justify-between w-full sticky text-teal-700 top-0 z-40 lg:border-b border-solid border-gray-150">
       {/** login/logout with wallet */}
       <div className="flex items-center sm:mr-2">
-        <MbButton onClick={buttonAction} label={buttonLabel} />
+        <MbButton
+          style={{ backgroundColor: "#083344", color: "#ccfbf1" }}
+          onClick={buttonAction}
+          label={buttonLabel}
+        />
       </div>
     </nav>
   );

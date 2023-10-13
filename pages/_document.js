13c4a9e113907457e7267417924df18e618f9a42
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-10-13 22:35:11 +0530

    chore: code cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-// import pkg from "../package.json";
-// import Footer from "components/footer";
-
 export default function Document() {
   return (
     <Html>

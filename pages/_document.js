807ac23d24e84cdac013fbc60d5cb84362b6793f
0xyshv <writetoyshv@gmail.com>
2023-10-13 22:40:50 +0530

    fix: sync

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-// import pkg from "../package.json";
-// import Footer from "components/footer";
-
 export default function Document() {
   return (
     <Html>
       <Head></Head>
-
       <body className="bg-teal-50">
         <Main />
-        {/* <Footer /> */}
         <NextScript />
       </body>
     </Html>

a204ac5c5155990915382764fb0936163049b095
0xyshv <writetoyshv@gmail.com>
2023-10-13 09:37:38 +0530

    feat: Add routing logic

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,14 @@
 import { useWallet } from "@mintbase-js/react";
 import { MbButton } from "mintbase-ui";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Navbar() {
   const { isConnected, connect, disconnect, activeAccountId } = useWallet();
+  const router = useRouter();
+
+  const isCreateNFTActive = router.pathname === "";
+  const isMarketplaceActive = router.pathname === "";
 
   function truncateString(str, num) {
     if (str.length <= num) {
@@ -19,20 +24,27 @@ function Navbar() {
   const buttonAction = isConnected ? disconnect : connect;
 
   return (
-    <nav className="flex justify-between w-full bg-white sticky top-0 z-40 lg:border-b border-solid border-gray-150">
-      <div className="flex justify-between items-center">
-        <h1 className="font-bold p-2 sm:p-4 border-gray-100">
-          NFT AI Artistry
-        </h1>
-        <div className="flex gap-3">
-          <Link href="/" className="text-black no-underline hover:underline">
-            Create NFT
-          </Link>
-          <Link href="/" className="text-black no-underline hover:underline">
-            Marketplace
-          </Link>
-        </div>
+    <nav className="flex justify-between items-center w-full bg-white sticky top-0 z-40 lg:border-b border-solid border-gray-150">
+      <h1 className="font-bold p-2 sm:p-4 border-gray-100">NFT AI Artistry</h1>
+      <div className="flex gap-3">
+        <Link
+          href="/"
+          className={`text-black no-underline px-4 py-2 rounded-full transition hover:shadow-lg ${
+            isCreateNFTActive ? "bg-gray-300" : "bg-white"
+          } hover:bg-gray-300`}
+        >
+          Create NFT
+        </Link>
+        <Link
+          href="/"
+          className={`text-black no-underline px-4 py-2 rounded-full transition hover:shadow-lg ${
+            isMarketplaceActive ? "bg-gray-300" : ""
+          } hover:bg-gray-300`}
+        >
+          Marketplace
+        </Link>
       </div>
+
       {/** login/logout with wallet */}
       <div className="flex items-center sm:mr-2">
         <MbButton onClick={buttonAction} label={buttonLabel} />
